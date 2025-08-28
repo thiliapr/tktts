3b20afe843eef206dfbe6ade60bb2348e3f7e291
thiliapr <140580293+thiliapr@users.noreply.github.com>
2025-08-28 14:54:01 +0800

    修复 FastSpeech2 中填充掩码的逻辑，确保在只在提供正向和负向提示时创建相应的掩码

--- utils/model.py
+++ utils/model.py
@@ -634,9 +634,9 @@ class FastSpeech2(nn.Module):
         # 如果没有提供填充掩码，则创建一个全为 False 的掩码
         if text_padding_mask is None:
             text_padding_mask = torch.zeros_like(text, dtype=torch.bool)  # [batch_size, text_len]
-        if positive_prompt_mask is None:
+        if positive_prompt is not None and positive_prompt_mask is None:
             positive_prompt_mask = torch.zeros_like(positive_prompt, dtype=torch.bool)  # [batch_size, num_pos_tags]
-        if negative_prompt_mask is None:
+        if negative_prompt is not None and negative_prompt_mask is None:
             negative_prompt_mask = torch.zeros_like(negative_prompt, dtype=torch.bool)  # [batch_size, num_neg_tags]
 
         # 词嵌入
